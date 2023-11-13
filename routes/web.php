881e78322cff345ed91b40a7c13436237db1cb8b
thanpawatpiti <128307763+thanpawatpiti@users.noreply.github.com>
2023-11-13 11:07:16 +0700

    remove frontend to vue

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-// For the restaurant-laravel-test page
-Route::get('/restaurant-laravel-test', [RestaurantLaravelTestController::class, 'index']);
